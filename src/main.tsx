fae88d21f45caeed04f1f703aa18844ab7ff807a
Jeampierre <ajeampiergg10@gmail.com>
2025-10-09 19:39:21 -0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,28 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.tsx'
 import './index.css'
+import logoLP from './assets/Logo LP.png'
+
+// Ensure favicon and title are set at runtime
+const ensureFaviconAndTitle = () => {
+  // Set title
+  if (document.title !== 'Learn Playing') {
+    document.title = 'Learn Playing'
+  }
+
+  // Set favicon
+  const head = document.head || document.getElementsByTagName('head')[0]
+  let link: HTMLLinkElement | null = document.querySelector("link[rel='icon']")
+  if (!link) {
+    link = document.createElement('link')
+    link.rel = 'icon'
+    head.appendChild(link)
+  }
+  link.type = 'image/png'
+  link.href = logoLP
+}
+
+ensureFaviconAndTitle()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
